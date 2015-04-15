1f3f3b9e9ed3e850d1829015054b34b5f938f284
tfilkins <tfilkins@KHO-IMSECUBU>
2015-04-15 12:31:20 -0500

    Added src/utils/utitlies.java to hold inherited methods.

---
+++ org.spdx.spdxeclipse/src/utils/utilities.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class utilities {
+	
+	// Finds the current workspace directory and returns it as a string; otherwise returns null
+	public String GetWorkspaceDirectory() {
+		
+		return null;
+	}
+	
+	
+
+}
