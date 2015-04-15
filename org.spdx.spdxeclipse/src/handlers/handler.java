3a86f3087c62a633a6d52d89e4e86452307ed25c
Ryan Beasley <ryanbeaz@gmail.com>
2015-04-15 15:59:52 -0500

    Updated the utilities method to grab the workbench location in eclipse.

--- org.spdx.spdxeclipse/src/handlers/handler.java
+++ org.spdx.spdxeclipse/src/handlers/handler.java
@@ -3,18 +3,20 @@ package handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-
 import utils.utilities;
 
 public class handler extends AbstractHandler {
 
 	public Object execute (ExecutionEvent event) throws ExecutionException {
-		String str = "whatever";
+		
 		utilities utils = new utilities();
+
+		String file = "helloworld.java";
 		
-		String filedir = utils.GetFileDirectory(str);
+		String filedir = utils.GetFileDirectory(file);
+		
 		System.out.printf("GetFileDirectory() : %s\n", filedir);
-		
+						
 		return null;
 	}
 }
--- org.spdx.spdxeclipse/src/utils/utilities.java
+++ org.spdx.spdxeclipse/src/utils/utilities.java
@@ -1,21 +1,22 @@
 package utils;
 
+import org.eclipse.core.resources.ResourcesPlugin;
+
 public class utilities {
 	
 	// Finds the current workspace directory and returns it as a string; otherwise returns null
 	public String GetWorkspaceDirectory() {
 		
-		// returns a local directory where project files are
-		String filepath = "filepath";
+		String workspacepath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
 		
-		return filepath;
+		return workspacepath;
 	}
 	
 	// Finds the directory path for the file given as a parameter
 	public String GetFileDirectory( String file ) {
 		String workspacedir = GetWorkspaceDirectory();
 		
-		return workspacedir;
+		return workspacedir + "/" + file;
 	}
 	
 	// validates that FOSSology is installed
