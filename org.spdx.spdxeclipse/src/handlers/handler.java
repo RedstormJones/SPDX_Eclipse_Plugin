3441de1c2af415fa8440ae4a98c388d22e5f70a0
TCF-30 <tfilkins@unomaha.edu>
2015-04-19 11:54:18 -0500

    Brough back GetWorkspaceDirectory() method and added code for creating spdx/ directory within the workspace directory.

--- org.spdx.spdxeclipse/src/handlers/handler.java
+++ org.spdx.spdxeclipse/src/handlers/handler.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-
 import utils.utilities;
 
 public class handler extends AbstractHandler {
@@ -14,21 +13,18 @@ public class handler extends AbstractHandler {
 	public Object execute (ExecutionEvent event) throws ExecutionException
 	{
 		utilities utils = new utilities();
-		String filepath = "file not found";
-		String tar_fp = null;
+		String filepath = null;
+		String tarpath = null;
 		
 		try { filepath = utils.GetFileAbsolutePath(); }
 		catch (FileNotFoundException e) { e.printStackTrace(); }
 		
-		if(filepath.equals(null)) { return null; }
-		else
-		{	
-			try { tar_fp = utils.PackageFile(filepath); }
-			catch (IOException e) { e.printStackTrace(); }
-			
-			System.out.printf("tar_fp:  %s\n", tar_fp);
-		}
+		try { tarpath = utils.PackageFile(filepath); }
+		catch (IOException e) {	e.printStackTrace(); }
 		
+		
+		System.out.printf("\nfilepath: %s\ntarpath: %s\n", filepath.toString(), tarpath);
+
 		
 		return null;
 	}
--- org.spdx.spdxeclipse/src/utils/utilities.java
+++ org.spdx.spdxeclipse/src/utils/utilities.java
@@ -1,30 +1,64 @@
 package utils;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.File;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 
 public class utilities {
 	
-	// Finds the directory path for the file given as a parameter
+	// Returns the absolute directory path of the currently open file in the editor
 	public String GetFileAbsolutePath() throws FileNotFoundException
 	{
 		String filepath = null;
+		
 		IWorkbenchPart workbenchpart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
 		IFile ifile = (IFile) workbenchpart.getSite().getPage().getActiveEditor().getEditorInput().getAdapter(IFile.class);
+		if(ifile == null) { throw new FileNotFoundException(); }
 		
-		if(ifile == null) {
-			throw new FileNotFoundException(); }
-		else {
-			filepath = ifile.getRawLocation().makeAbsolute().toOSString(); }
+		filepath = ifile.getRawLocation().makeAbsolute().toOSString();
+		if(filepath.equals(null)) { throw new FileNotFoundException(); }
 		
 		return filepath;
 	}
+	
+	
+	// Returns the workspace directory
+	public IPath GetWorkspaceDirectory() throws FileNotFoundException
+	{
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IPath wksp_dir = workspace.getRoot().getLocation();
+		
+		if(wksp_dir == null) { throw new FileNotFoundException(); }
+		
+		return wksp_dir;
+	}
+	
+	
+	// Gets the workspace directory and creates a spdx/ directory for storing
+	// the spdx documents. Then executes library functions for creating a .tar
+	// file of the file specified in the filepath parameter.
+	public String PackageFile( String filepath ) throws IOException
+	{
+		IPath wksp_dir = null;
+		
+		try { wksp_dir = GetWorkspaceDirectory(); }
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+		
+		String tar_fp = wksp_dir.append("/spdx").toOSString();
+		File temp = new File(tar_fp);
+		if(!temp.mkdir()) { System.out.println("creating <workspace>/spdx directory failed"); }
+
+		
+		return tar_fp;
+	}
+	
 	
 	// validates that FOSSology is installed
 	public Boolean ValidateFOSSology() {
@@ -32,38 +66,11 @@ public class utilities {
 		return true;
 	}
 	
+	
 	// validates that the DoSPDX.py ran successfully
 	public Boolean ValidateDoSOCS() {
 		
 		return true;
-	}
-	
-	// packages the current file to be sent to DoSOCS into a .tar file
-	public String PackageFile( String filepath ) throws IOException
-	{
-		String tarcmd = null;
-		String tar_fp = null;
-		String line;
-		
-		if(filepath.equals(null)) { return null; }
-		else {
-			tar_fp = filepath + ".tar";
-			tarcmd = "gksudo 'tar -cf " + tar_fp + " " + filepath + "'";
-			System.out.printf("\ntarcmd:  %s\n", tarcmd);
-		}
-		
-		Process tarproc = Runtime.getRuntime().exec(tarcmd);
-		
-		//BufferedReader stdoutread = new BufferedReader (new InputStreamReader(tarproc.getInputStream()));
-		//while ((line = stdoutread.readLine()) != null) {;}
-		
-		//BufferedReader stderrRead = new BufferedReader (new InputStreamReader(tarproc.getErrorStream()));
-		//while ((line = stderrRead.readLine()) != null) {;}
-		
-		//int retval = tarproc.exitValue();
-		//if(Runtime.getRuntime().exec(tarcmd) == null) { return "Failed to create .tar file"; }
-		
-		return tar_fp;
 	}
 	
 }
