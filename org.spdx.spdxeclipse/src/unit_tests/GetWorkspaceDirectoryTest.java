fd7404e146f0e36dc8f303fa577029c780c47d1b
Ryan Beasley <ryanbeaz@gmail.com>
2015-04-19 14:34:57 -0500

    Added the GetWorkspaceDirectoryTest.java unit test class.

---
+++ org.spdx.spdxeclipse/src/unit_tests/GetWorkspaceDirectoryTest.java
@@ -0,0 +1,13 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GetWorkspaceDirectoryTest {
+	
+	@Test
+	public void helloworld() {
+		fail("Not yet implemented");
+	}
+}
