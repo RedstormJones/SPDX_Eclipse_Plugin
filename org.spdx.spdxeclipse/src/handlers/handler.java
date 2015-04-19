9b9b9f51e6fadb840ef3d3090f9ade78c34e20bd
TCF-30 <tfilkins@unomaha.edu>
2015-04-19 01:50:50 -0500

    progress on PackageFile() utility method

--- org.spdx.spdxeclipse/src/handlers/handler.java
+++ org.spdx.spdxeclipse/src/handlers/handler.java
@@ -1,6 +1,7 @@
 package handlers;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -13,16 +14,21 @@ public class handler extends AbstractHandler {
 	public Object execute (ExecutionEvent event) throws ExecutionException
 	{
 		utilities utils = new utilities();
-		String filepath = "file-not-found";
+		String filepath = "file not found";
+		String tar_fp = null;
 		
-		try {
-			filepath = utils.GetFileAbsolutePath(); 
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
+		try { filepath = utils.GetFileAbsolutePath(); }
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+		
+		if(filepath.equals(null)) { return null; }
+		else
+		{	
+			try { tar_fp = utils.PackageFile(filepath); }
+			catch (IOException e) { e.printStackTrace(); }
+			
+			System.out.printf("tar_fp:  %s\n", tar_fp);
 		}
 		
-		System.out.printf("GetFileDirectory() : %s\n", filepath);
 		
 		return null;
 	}
--- org.spdx.spdxeclipse/src/utils/utilities.java
+++ org.spdx.spdxeclipse/src/utils/utilities.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.ui.IWorkbenchPart;
@@ -9,7 +12,7 @@ import org.eclipse.ui.PlatformUI;
 public class utilities {
 	
 	// Finds the directory path for the file given as a parameter
-	public String GetFileAbsolutePath( ) throws FileNotFoundException
+	public String GetFileAbsolutePath() throws FileNotFoundException
 	{
 		String filepath = null;
 		IWorkbenchPart workbenchpart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
@@ -17,8 +20,8 @@ public class utilities {
 		
 		if(ifile == null) {
 			throw new FileNotFoundException(); }
-		
-		filepath = ifile.getRawLocation().makeAbsolute().toOSString();
+		else {
+			filepath = ifile.getRawLocation().makeAbsolute().toOSString(); }
 		
 		return filepath;
 	}
@@ -36,9 +39,31 @@ public class utilities {
 	}
 	
 	// packages the current file to be sent to DoSOCS into a .tar file
-	public String PackageFile( String file ) {
+	public String PackageFile( String filepath ) throws IOException
+	{
+		String tarcmd = null;
+		String tar_fp = null;
+		String line;
 		
-		return null;
+		if(filepath.equals(null)) { return null; }
+		else {
+			tar_fp = filepath + ".tar";
+			tarcmd = "gksudo 'tar -cf " + tar_fp + " " + filepath + "'";
+			System.out.printf("\ntarcmd:  %s\n", tarcmd);
+		}
+		
+		Process tarproc = Runtime.getRuntime().exec(tarcmd);
+		
+		//BufferedReader stdoutread = new BufferedReader (new InputStreamReader(tarproc.getInputStream()));
+		//while ((line = stdoutread.readLine()) != null) {;}
+		
+		//BufferedReader stderrRead = new BufferedReader (new InputStreamReader(tarproc.getErrorStream()));
+		//while ((line = stderrRead.readLine()) != null) {;}
+		
+		//int retval = tarproc.exitValue();
+		//if(Runtime.getRuntime().exec(tarcmd) == null) { return "Failed to create .tar file"; }
+		
+		return tar_fp;
 	}
 	
 }
