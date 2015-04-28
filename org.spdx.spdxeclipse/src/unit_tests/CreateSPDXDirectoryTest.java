4af6028e3554e4e85099eb7deb546e06567d1afa
Ryan Beasley <ryanbeaz@gmail.com>
2015-04-28 17:57:15 -0500

    Added Unit Tests

---
+++ org.spdx.spdxeclipse/src/unit_tests/CreateSPDXDirectoryTest.java
@@ -0,0 +1,14 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreateSPDXDirectoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ org.spdx.spdxeclipse/src/unit_tests/CreateTarballTest.java
@@ -0,0 +1,14 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreateTarballTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
