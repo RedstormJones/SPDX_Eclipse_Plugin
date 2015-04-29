55d9513308504a66a112d994d55584af33b90cee
Ryan Beasley <ryanbeaz@gmail.com>
2015-04-28 20:12:45 -0500

    Updates.

--- org.spdx.spdxeclipse/src/classes/ExceptionUtilities.java
+++ org.spdx.spdxeclipse/src/classes/ExceptionUtilities.java
@@ -1,5 +1,22 @@
 package classes;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+
 public class ExceptionUtilities {
 	
+	public void Error()
+	{
+		MessageDialog.openError(Display.getCurrent().getActiveShell(), "ERROR", "There was an error while generating your SPDX Document.  Please try your request again.");
+	}
+	
+	public void Error(String message)
+	{
+		MessageDialog.openError(Display.getCurrent().getActiveShell(), "ERROR", message);
+	}
+	
+	public void Error(String message, Throwable e)
+	{
+		
+	}
 }
