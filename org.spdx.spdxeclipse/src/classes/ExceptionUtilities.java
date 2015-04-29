3d717e0866239dbe2666a352a060fafa5f3e6161
Ryan Beasley <ryanbeaz@gmail.com>
2015-04-28 20:21:23 -0500

    Updates

--- org.spdx.spdxeclipse/src/classes/ExceptionUtilities.java
+++ org.spdx.spdxeclipse/src/classes/ExceptionUtilities.java
@@ -1,5 +1,8 @@
 package classes;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 
@@ -17,6 +20,19 @@ public class ExceptionUtilities {
 	
 	public void Error(String message, Throwable e)
 	{
+		Status status = new Status(IStatus.ERROR, "org.spdx.spdxeclipse", 0, message, e);
+		ErrorDialog dlg = new ErrorDialog(Display.getCurrent().getActiveShell(), "ERROR", message, status, IStatus.ERROR);
 		
+		dlg.open();
+	}
+	
+	public void Warning()
+	{
+		MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "WARNING", "There was a warning while generating your SPDX Document.  Please try your request again.");
+	}
+	
+	public void Warning(String message)
+	{
+		MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "WARNING", message);
 	}
 }
