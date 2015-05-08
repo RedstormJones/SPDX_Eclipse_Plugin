8b87938dcba480b8da45606d384dc9e83e20a6f5
Ryan Beasley <ryanbeaz@gmail.com>
2015-05-08 09:15:03 -0500

    Updates

--- org.spdx.spdxeclipse/src/unit_tests/UtilitiesTest/AllTests.java
+++ org.spdx.spdxeclipse/src/unit_tests/UtilitiesTest/AllTests.java
@@ -6,15 +6,9 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 
-@SuiteClasses({ CreateSPDXDirectoryTest.class, 
-				CreateSPDXTest.class,
-				CreateTarballTest.class, 
-				GetFileAbsolutePathTest.class,
-				GetOpenFilenameTest.class, 
-				GetProjectDirectoryTest.class,
-				GetWorkspaceDirectoryTest.class, 
-				RefreshInstanceTest.class })
+@SuiteClasses({ GetWorkspaceDirectoryTest.class })
 
-public class AllTests {
+public class AllTests 
+{
 
 }
