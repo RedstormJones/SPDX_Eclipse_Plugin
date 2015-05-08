d7a01ff756600a6f6a8c14a668115bb5095f7f3c
Ryan Beasley <ryanbeaz@gmail.com>
2015-05-08 00:06:36 -0500

    Updates

--- org.spdx.spdxeclipse/src/unit_tests/UtilitiesTest/GetProjectDirectoryTest.java
+++ org.spdx.spdxeclipse/src/unit_tests/UtilitiesTest/GetProjectDirectoryTest.java
@@ -1,14 +1,18 @@
 package unit_tests.UtilitiesTest;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class GetProjectDirectoryTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void GetProjectDirectoryTest_Pass() 
+	{
+		
 	}
-
+	
+	@Test
+	public void GetProjectDirectoryTest_Fail() 
+	{
+		
+	}
 }
